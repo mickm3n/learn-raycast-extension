a41aeab1c71be7f334eed753bb1bdff672de986c
Mick Zhuang <mickzhuang@gmail.com>
2025-01-08 15:02:38 +0800

    change template to store sparks

---
+++ src/add-sparks.tsx
@@ -0,0 +1,5 @@
+import SparkForm from "./form";
+
+export default function AddSpark() {
+  return <SparkForm />;
+}
--- src/form.tsx
+++ src/form.tsx
@@ -1,51 +1,27 @@
-import { Action, ActionPanel, Form, showToast, Toast, useNavigation } from "@raycast/api";
+import { Action, ActionPanel, Form, showToast, Toast } from "@raycast/api";
 import { FormValidation, useForm } from "@raycast/utils";
-import { validate } from "email-validator";
-import { createUser, updateUser, User } from "./api";
+import { createSpark } from "./api";
 
 type FormValues = {
-  email: string;
-  firstName: string;
-  lastName: string;
+  thought: string;
 };
 
-export default function UserForm(props: { user?: User; title?: string; onPop?: () => void }) {
-  const { pop } = useNavigation();
-  const { itemProps, handleSubmit, reset, focus } = useForm<FormValues>({
-    initialValues: props.user,
+export default function SparkForm() {
+  const { itemProps, handleSubmit, reset } = useForm<FormValues>({
     validation: {
-      email(value) {
-        return value && validate(value) ? undefined : "The email is invalid";
-      },
-      firstName: FormValidation.Required,
-      lastName: FormValidation.Required,
+      thought: FormValidation.Required,
     },
     async onSubmit(values) {
-      await showToast({ style: Toast.Style.Animated, title: props.user ? "Updating user" : "Creating user" });
+      await showToast({ style: Toast.Style.Animated, title: "記錄想法中..." });
 
       try {
-        if (props.user) {
-          await updateUser(props.user, { email: values.email, firstName: values.firstName, lastName: values.lastName });
-          await showToast({ style: Toast.Style.Success, title: "Updated user" });
-
-          if (props.user) {
-            if (props.onPop) {
-              props.onPop();
-            }
-
-            pop();
-          }
-        } else {
-          await createUser(values.email, values.firstName, values.lastName);
-          await showToast({ style: Toast.Style.Success, title: "Created user" });
-
-          reset();
-          focus("email");
-        }
+        await createSpark(values.thought);
+        await showToast({ style: Toast.Style.Success, title: "已記錄想法" });
+        reset();
       } catch (error) {
         await showToast({
           style: Toast.Style.Failure,
-          title: props.user ? "Failed updating user" : "Failed creating user",
+          title: "記錄失敗",
           message: error instanceof Error ? error.message : String(error),
         });
       }
@@ -54,16 +30,19 @@ export default function UserForm(props: { user?: User; title?: string; onPop?: (
 
   return (
     <Form
-      navigationTitle={props.title}
       actions={
         <ActionPanel>
-          <Action.SubmitForm title={props.user ? "Update User" : "Create User"} onSubmit={handleSubmit} />
+          <Action.SubmitForm title="記錄想法" onSubmit={handleSubmit} />
         </ActionPanel>
       }
     >
-      <Form.TextField title="Email" placeholder="ada@lovelace.com" autoFocus {...itemProps.email} />
-      <Form.TextField title="First Name" placeholder="Ada" {...itemProps.firstName} />
-      <Form.TextField title="Last Name" placeholder="Lovelace" {...itemProps.lastName} />
+      <Form.TextArea
+        id="thought"
+        title="想法"
+        placeholder="寫下你的想法..."
+        autoFocus
+        {...itemProps.thought}
+      />
     </Form>
   );
 }
